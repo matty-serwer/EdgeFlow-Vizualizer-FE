d9d4236c67b1f76931e222e7cfc14dda7751e720
Matty Serwer <Matthew.Serwer@gmail.com>
2025-04-17 14:37:44 -0400

    Creates stompClient; a reusable WebSocket client module, which connets to /ws, and subscribes to /topic/data on the backend. Rewrites App.tsx to be a basic display for handling data from stomp. Creates the Dashboard which displays the data using material Card components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { connectStomp, disconnectStomp } from "./utils/stompClient";
+import { Container, Typography } from "@mui/material";
+// Components
+import Dashboard from "./coponents/Dashboard";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [data, setData] = useState<any | null>(null);
+
+  useEffect(() => {
+    connectStomp(setData);
+    return () => disconnectStomp();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container maxWidth="sm" style={{ marginTop: '2rem' }}>
+      <Typography variant="h4" gutterBottom>
+        EdgeFlow Daashboard
+      </Typography>
+      <Dashboard data={data} />
+    </Container>
   )
 }
-
-export default App
---
+++ src/utils/stompClient.ts
@@ -0,0 +1,34 @@
+import SockJS from 'sockjs-client';
+import { Client, IMessage, StompSubscription } from '@stomp/stompjs';
+
+const URL = import.meta.env.VITE_WS_URL;
+
+let stompClient: Client | null = null;
+
+export const connectStomp = (onMessage: (data: any) => void) => {
+  stompClient = new Client({
+    webSocketFactory: () => new SockJS(URL),
+    reconnectDelay: 3000,
+    onConnect: () => {
+      console.log('[WS] Connected');
+      stompClient?.subscribe('/topic/data', (message: IMessage) => {
+        const payload = JSON.parse(message.body);
+        onMessage(payload);
+      });
+    },
+    onStompError: (frame) => {
+      console.error('[WS] Broker error:', frame.headers['message']);
+    },
+    onWebSocketError: (err) => {
+      console.error('[WS] Connection error:', err);
+    },
+  });
+
+  stompClient.activate();
+};
+
+export const disconnectStomp = () => {
+  if (stompClient?.connected) {
+    stompClient.deactivate();
+  }
+}
